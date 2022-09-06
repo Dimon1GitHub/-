420713d2502ccdf129ab33929c468498e68eeaf4
Dimon1GitHub <111465123+Dimon1GitHub@users.noreply.github.com>
2022-09-06 16:01:36 +0300

    Add files via upload

---
+++ config1.py
@@ -0,0 +1,10 @@
+
+#BOT:
+TOKEN = "5402402441:AAFpetusOV2vt31BoiQs5pIfjVKP3xjvDnE" # ТОКЕН БОТА
+ADMIN = 5372794653 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # 
+API_ID = 18544388
+API_HASH = "5a93f82437f9e1e186735a5217624de0"
